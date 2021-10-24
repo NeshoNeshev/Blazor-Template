32656cae1c5b027b3e1a90c49735fb6d7d54d2bd
NeshoNeshev <69240150+NeshoNeshev@users.noreply.github.com>
2021-10-24 18:35:07 +0300

    Implement SendGrid

---
+++ Services/BlazorWebAssembly.Services.Messaging/Interfaces/IEmailSender.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace BlazorWebAssembly.Services.Messaging.Interfaces
+{
+    public interface IEmailSender
+    {
+        Task SendEmailAsync(
+            string from,
+            string fromName,
+            string to,
+            string subject,
+            string htmlContent,
+            IEnumerable<EmailAttachment> attachments = null);
+    }
+}
---
+++ Services/BlazorWebAssembly.Services.Messaging/NullMessageSender.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace BlazorWebAssembly.Services.Messaging
+{
+    public class NullMessageSender
+    {
+        public Task SendEmailAsync(
+            string from,
+            string fromName,
+            string to,
+            string subject,
+            string htmlContent,
+            IEnumerable<EmailAttachment> attachments = null)
+        {
+            return Task.CompletedTask;
+        }
+    }
+}
