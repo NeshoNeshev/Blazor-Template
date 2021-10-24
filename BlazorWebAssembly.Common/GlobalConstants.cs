1cd1e835dc5787aaab38419b23c9eec1e8241492
NeshoNeshev <69240150+NeshoNeshev@users.noreply.github.com>
2021-10-24 18:45:33 +0300

    Add Common project

---
+++ BlazorWebAssembly.Common/GlobalConstants.cs
@@ -0,0 +1,17 @@
+﻿namespace BlazorWebAssembly.Common
+{
+    public static class GlobalConstants
+    {
+        public const string ApplicationName = "TemplateBlazor";
+
+        public const string AdministratorRoleName = "Administrator";
+
+        public const string JsonContentType = "application/json";
+
+        public const string AdministratorEmail = "admin@app.com";
+
+        public const string AdministratorUserName = "admin@app.com";
+
+        public const string AdministratorPassword = "Administrator123";
+    }
+}
